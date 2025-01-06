91d7d803c039d2da7359b3f3b6640a902320bac6
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-01-06 19:23:59 +0530

    Time: 55 ms (45.91%), Space: 12.2 MB (40.56%) - LeetHub

---
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        vector<int> ans(boxes.length());
+        for (int i=0;i<boxes.length();i++){
+            if (boxes[i]=='1'){
+                for (int j=0;j<boxes.length();j++){
+                    ans[j]+=(abs(j-i));
+                }
+            }
+        }
+        return ans;
+    }
+};
