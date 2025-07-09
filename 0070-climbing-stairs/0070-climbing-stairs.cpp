52de226081d9daffec9478ccd013bc0b72ee11e9
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-07-09 22:55:34 +0530

    Time: 0 ms (100%), Space: 8.5 MB (36.77%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
+        // Iterative
+        // if (n == 1) return 1;
+        // if (n == 2) return 2;
+        // int a = 1; 
+        // int b = 2; 
+        // int c;
+        // for (int i = 3; i <= n; i++) {
+        //     c = a + b;
+        //     a = b;
+        //     b = c;
+        // }
+        // return b;
 
-        int a = 1; 
-        int b = 2; 
-        int c;
+        //Recursion (TLE)
+        // if (n==0 || n==1){
+        //     return 1;
+        // }
+        // return climbStairs(n-1)+climbStairs(n-2);
+        
 
-        for (int i = 3; i <= n; i++) {
-            c = a + b;
-            a = b;
-            b = c;
-        }
-
-        return b;
+        //Memoization
+        vector<int> dp(n+1,-1);
+        return memoization(n, dp);
     }
+     int memoization(int n, vector<int>&dp){
+        if(n==1||n==0){
+            return 1;
+        }
+        if (dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=memoization(n-1,dp)+memoization(n-2,dp);
+    }
+    
 };
