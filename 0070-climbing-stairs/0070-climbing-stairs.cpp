bfc85fd2b63d6c5be801ff7664076d7c340b8920
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-07-10 00:22:59 +0530

    Time: 0 ms (100%), Space: 8.7 MB (18.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -22,8 +22,17 @@ public:
         
 
         //Memoization
-        vector<int> dp(n+1,-1);
-        return memoization(n, dp);
+        // vector<int> dp(n+1,-1);
+        // return memoization(n, dp);
+
+        //Tabulation
+        vector<int> vec(n+1,0);
+        vec[0]=1;
+        vec[1]=1;
+        for (int i=2;i<=n;i++){
+            vec[i]=vec[i-1]+vec[i-2];
+        }
+        return vec[n];
     }
      int memoization(int n, vector<int>&dp){
         if(n==1||n==0){
