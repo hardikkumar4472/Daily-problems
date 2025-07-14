b32af27be822fea33e5249857121007197ff0ef8
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-07-14 13:37:16 +0530

    Time: 8 ms (79.85%), Space: 19 MB (99.23%) - LeetHub

--- 2317-count-collisions-on-a-road/2317-count-collisions-on-a-road.cpp
+++ 2317-count-collisions-on-a-road/2317-count-collisions-on-a-road.cpp
@@ -1,36 +1,51 @@
 class Solution {
 public:
-    int countCollisions(string s) {
-        stack<char> highway; 
-        int collisions = 0;
+    int countCollisions(string directions) {
+        // stack<char> highway; 
+        // int collisions = 0;
         
-        for (char car : s) {
-            if (!highway.empty() && highway.top() == 'R' && car == 'L') {
-                collisions += 2;
-                highway.pop();     
-                highway.push('S'); 
-            }
-            else if (!highway.empty() && highway.top() == 'R' && car == 'S') {
-                collisions += 1;
-                highway.pop();     
-                highway.push('S'); 
-            }
-            else if (!highway.empty() && highway.top() == 'S' && car == 'L') {
-                collisions += 1;   
-            }
-            else {
-                highway.push(car);
+        // for (char car : s) {
+        //     if (!highway.empty() && highway.top() == 'R' && car == 'L') {
+        //         collisions += 2;
+        //         highway.pop();     
+        //         highway.push('S'); 
+        //     }
+        //     else if (!highway.empty() && highway.top() == 'R' && car == 'S') {
+        //         collisions += 1;
+        //         highway.pop();     
+        //         highway.push('S'); 
+        //     }
+        //     else if (!highway.empty() && highway.top() == 'S' && car == 'L') {
+        //         collisions += 1;   
+        //     }
+        //     else {
+        //         highway.push(car);
+        //     }
+        // }
+        
+        // while (!highway.empty() && highway.top() == 'R') {
+        //     highway.pop();
+        // }
+        // while (!highway.empty()) {
+        //     if (highway.top() == 'R') collisions++;
+        //     highway.pop();
+        // }
+        
+        // return collisions;
+        int cnt = 0;
+        int n = directions.size();
+        int i = 0, j = n-1;
+        while(i<n && directions[i]=='L'){
+            i++;
+        }
+        while(j>=0 && directions[j]=='R'){
+            j--;
+        }
+        for(int k = i; k<=j; k++){
+            if(directions[k]!='S'){
+                cnt++;
             }
         }
-        
-        while (!highway.empty() && highway.top() == 'R') {
-            highway.pop();
-        }
-        while (!highway.empty()) {
-            if (highway.top() == 'R') collisions++;
-            highway.pop();
-        }
-        
-        return collisions;
+        return cnt;
     }
 };
