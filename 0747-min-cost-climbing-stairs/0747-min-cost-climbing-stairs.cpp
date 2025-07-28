7466b7a438c6e5a3842bba20dc4b75a089f7a24f
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-07-28 10:53:35 +0530

    Time: 0 ms (100%), Space: 17.7 MB (30.77%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,10 +1,43 @@
 class Solution {
 public:
+    // int recursion(vector<int>& cost, int i){
+    //     if (i<0){
+    //         return 0;
+    //     }
+    //     if (i==0 || i==1){
+    //         return cost[i];
+    //     }
+    //     return cost[i]+min(recursion(cost,i-1), recursion(cost,i-2));
+    // }
+
+    int memoization(vector<int>& cost, vector<int>& dp, int i) {
+        if (i < 0) return 0;
+        if (i == 0 || i == 1) return cost[i];
+        if (dp[i] != -1) return dp[i];
+        
+        dp[i] = cost[i] + min(memoization(cost, dp, i-1), memoization(cost, dp, i-2));
+        return dp[i];
+    }
+
     int minCostClimbingStairs(vector<int>& cost) {
         //iterative (Not DP)
-        for (int i=2;i<cost.size();i++){
-            cost[i]+=min(cost[i-1],cost[i-2]);
-        }
-        return min(cost[cost.size()-1],cost[cost.size()-2]);
+        // for (int i=2;i<cost.size();i++){
+        //     cost[i]+=min(cost[i-1],cost[i-2]);
+        // }
+        // return min(cost[cost.size()-1],cost[cost.size()-2]);
+    
+
+        // recursion [Time Limit Exceeded]
+        // int n=cost.size();
+        // return min(recursion(cost,n-1),recursion(cost,n-2));
+
+        //Memoization
+        int n = cost.size();
+        vector<int> dp(n, -1); 
+        return min(memoization(cost, dp, n-1), memoization(cost, dp, n-2));
+
+        
+
+
     }
 };
