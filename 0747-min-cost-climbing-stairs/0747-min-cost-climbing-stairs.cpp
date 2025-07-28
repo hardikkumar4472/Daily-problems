d6e15308e7d5eab4ecb4fdb98eb0125c44aa8fd9
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-07-28 11:00:04 +0530

    Time: 0 ms (100%), Space: 17.5 MB (89.66%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    // int recursion(vector<int>& cost, int i){
-    //     if (i<0){
-    //         return 0;
-    //     }
-    //     if (i==0 || i==1){
-    //         return cost[i];
-    //     }
-    //     return cost[i]+min(recursion(cost,i-1), recursion(cost,i-2));
-    // }
+    int recursion(vector<int>& cost, int i){
+        if (i<0){
+            return 0;
+        }
+        if (i==0 || i==1){
+            return cost[i];
+        }
+        return cost[i]+min(recursion(cost,i-1), recursion(cost,i-2));
+    }
 
     int memoization(vector<int>& cost, vector<int>& dp, int i) {
-        if (i < 0) return 0;
-        if (i == 0 || i == 1) return cost[i];
-        if (dp[i] != -1) return dp[i];
+        if (i < 0){
+            return 0;
+        }
+        if (i == 0 || i == 1) {
+            return cost[i];
+        }
+        if (dp[i] != -1){
+            return dp[i];
+        }
         
         dp[i] = cost[i] + min(memoization(cost, dp, i-1), memoization(cost, dp, i-2));
         return dp[i];
@@ -32,12 +38,22 @@ public:
         // return min(recursion(cost,n-1),recursion(cost,n-2));
 
         //Memoization
+        // int n = cost.size();
+        // vector<int> dp(n, -1); 
+        // return min(memoization(cost, dp, n-1), memoization(cost, dp, n-2));
+
+        // Tabulation(Iterative DP)
         int n = cost.size();
-        vector<int> dp(n, -1); 
-        return min(memoization(cost, dp, n-1), memoization(cost, dp, n-2));
+        vector<int> dp(n, 0);
 
-        
+        dp[0] = cost[0];
+        dp[1] = cost[1];
 
+        for (int i = 2; i < n; i++) {
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+        }
+
+        return min(dp[n-1], dp[n-2]);
 
     }
 };
