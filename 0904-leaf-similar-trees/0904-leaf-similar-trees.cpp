bb1d89047a1b856cc199c25bc2987b9f974b8060
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-07-29 15:01:49 +0530

    Time: 0 ms (100%), Space: 15.2 MB (69.86%) - LeetHub

---
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void getLeaves(TreeNode* root, vector<int>& leaves) {
+        if (!root) return;
+        if (!root->left && !root->right) {
+            leaves.push_back(root->val);
+        }
+        getLeaves(root->left, leaves);
+        getLeaves(root->right, leaves);
+    }
+
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> leaves1, leaves2;
+        getLeaves(root1, leaves1);
+        getLeaves(root2, leaves2);
+        return leaves1 == leaves2;
+    }
+};
