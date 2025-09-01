5f11ebad190bae31e97382d892cc36a588b94b2c
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-09-01 23:08:54 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.09%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string str=to_string(n);
+        if (!next_permutation(str.begin(),str.end())){
+            return -1;
+        }
+        long long s=stoll(str);
+        if (s>INT_MAX){
+            return -1;
+        }
+        return s;
+    }
+};
