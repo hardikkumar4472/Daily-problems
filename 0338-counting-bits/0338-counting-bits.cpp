cbbcecaa8c293edaccc1e4cdbb6c9dfcc1031834
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-11-02 19:09:33 +0530

    Time: 0 ms (100%), Space: 10.9 MB (34.92%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    string binary(int n){
-        string ans="";
-        while (n!=0){
-            if (n%2==0){
-                ans+='0';
-            }
-            else if (n%2!=0){
-                ans+='1';
-            }
-            n/=2;
-        }
-        ans+=1;
-        return ans;
-    }
+    // string binary(int n){
+    //     string ans="";
+    //     while (n!=0){
+    //         if (n%2==0){
+    //             ans+='0';
+    //         }
+    //         else if (n%2!=0){
+    //             ans+='1';
+    //         }
+    //         n/=2;
+    //     }
+    //     ans+=1;
+    //     return ans;
+    // }
     // int getCount(string s){
     //     int ct=0;
     //     for (char c: s){
@@ -23,16 +23,27 @@ public:
     //     }
     //     return ct;
     // }
-    int recursion(int n){
+    // int recursion(int n){
+    //     if (n==0){
+    //         return 0;
+    //     }
+    //     return (n%2)+recursion(n/2);
+    // }
+    int memo(int n, vector<int> &dp){
         if (n==0){
             return 0;
         }
-        return (n%2)+recursion(n/2);
+        if (dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=(n%2)+memo(n/2,dp);
+
     }
     vector<int> countBits(int n) {
-        vector<int> ans;
+        vector<int> dp(n+1,-1);
+        vector<int> ans(n+1);
         for (int i=0;i<=n;i++){
-            ans.push_back(recursion(i));
+            ans[i]=memo(i,dp);
         }
         return ans;
     }
