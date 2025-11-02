d00b13f0d99589077df085f185af98bbb2d9d824
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-11-02 19:02:54 +0530

    Time: 5 ms (7.21%), Space: 11.5 MB (9%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -14,20 +14,25 @@ public:
         ans+=1;
         return ans;
     }
-    int getCount(string s){
-        int ct=0;
-        for (char c: s){
-            if (c=='1'){
-                ct+=1;
-            }
+    // int getCount(string s){
+    //     int ct=0;
+    //     for (char c: s){
+    //         if (c=='1'){
+    //             ct+=1;
+    //         }
+    //     }
+    //     return ct;
+    // }
+    int recursion(int n){
+        if (n==0){
+            return 0;
         }
-        return ct;
+        return (n%2)+recursion(n/2);
     }
-
     vector<int> countBits(int n) {
         vector<int> ans;
         for (int i=0;i<=n;i++){
-            ans.push_back(getCount(binary(i)));
+            ans.push_back(recursion(i));
         }
         return ans;
     }
