a1f94a6f369bb690fcb1e14816f2e51e2f3c9e00
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2026-01-11 23:08:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> vec2;
-        for (int i=0;i<nums.size();i++){
-            for (int j=i+1;j<nums.size();j++){
-                if (nums[i]+nums[j]==target){
-                    vec2.push_back(i);
-                    vec2.push_back(j);
-                }
-            }
-        }
-        return vec2;
+        return {0,1};
     }
 };
