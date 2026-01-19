f854b3892e6da27c40771ae8bf9eaf9e2f6f7e9f
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2026-01-19 16:03:10 +0530

    Time: 0 ms (100%), Space: 8.5 MB (34.15%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,47 +1,33 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        // Iterative
-        // if (n == 1) return 1;
-        // if (n == 2) return 2;
-        // int a = 1; 
-        // int b = 2; 
-        // int c;
-        // for (int i = 3; i <= n; i++) {
-        //     c = a + b;
-        //     a = b;
-        //     b = c;
-        // }
-        // return b;
+    // int memo(int n, vector<int> &dp){
+    //     if (n==0 || n==1){
+    //         return 1;
+    //     }
+    //     if (dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     return dp[n]=memo(n-1,dp)+memo(n-2,dp);
+    // }
 
-        //Recursion (TLE)
+    int climbStairs(int n) {
+        //Recursion
         // if (n==0 || n==1){
         //     return 1;
-        // }
+        // };
         // return climbStairs(n-1)+climbStairs(n-2);
-        
-
-        //Memoization
-        // vector<int> dp(n+1,-1);
-        // return memoization(n, dp);
 
         //Tabulation
-        vector<int> vec(n+1,0);
-        vec[0]=1;
-        vec[1]=1;
-        for (int i=2;i<=n;i++){
-            vec[i]=vec[i-1]+vec[i-2];
+        // vector<int> dp(n+1, -1);
+        // return memo(n,dp);
+
+        //Memoization
+        vector<int> dp(n+1,-1);
+        dp[0]=1;
+        dp[1]=1;
+        for (int i=2;i< dp.size();i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return vec[n];
+        return dp[n];
     }
-     int memoization(int n, vector<int>&dp){
-        if(n==1||n==0){
-            return 1;
-        }
-        if (dp[n]!=-1){
-            return dp[n];
-        }
-        return dp[n]=memoization(n-1,dp)+memoization(n-2,dp);
-    }
-    
 };
