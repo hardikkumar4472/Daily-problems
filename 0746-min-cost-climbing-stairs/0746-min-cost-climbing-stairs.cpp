b4d4c5054fbeb599758b82469c52f7a046e68814
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2026-01-20 15:54:49 +0530

    Time: 0 ms (100%), Space: 17.7 MB (52.2%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    // int recursion(vector<int>& cost, int i){
+    //     if (i<0){
+    //         return 0;
+    //     }
+    //     if (i==0 || i==1){
+    //         return cost[i];
+    //     }
+    //     return cost[i]+min(recursion(cost,i-1),recursion(cost,i-2));
+    // }
+    int memoization(vector<int> &dp, int i, vector<int> &cost){
+        if (i==0 || i==1){
+            return cost[i];
+        }
+        if (dp[i]!=-1){
+            return dp[i];
+        }
+        return dp[i]=cost[i]+min(memoization(dp,i-1,cost), memoization(dp,i-2,cost));
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        // return min(recursion(cost, cost.size()-1),recursion(cost,cost.size()-2));
+
+        //Memoization
+        vector<int> dp(cost.size()+1,-1);
+        return min(memoization(dp,cost.size()-1,cost),memoization(dp,cost.size()-2,cost));
+
+    }
+};
