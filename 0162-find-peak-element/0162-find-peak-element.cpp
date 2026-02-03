210bac06712220ab2816b2e42bc3355fe9a944aa
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2026-02-03 09:37:20 +0530

    Time: 0 ms (100%), Space: 12.5 MB (87.19%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
-        if (nums.size()==0){
-            return -1;
+    int recursionBinary(vector<int>& nums, int low, int high){
+        if (low==high){
+            return low;
         }
-        if (nums.size()==1 || nums[0]>nums[1]){
-            return 0;
+        int mid=low+(high-low)/2;
+        if (nums[mid]>nums[mid+1]){
+           return recursionBinary(nums, low, mid);
         }
-
-        if (nums[nums.size()-1]>nums[nums.size()-2]){
-            return nums.size()-1;
-        }
-        for (int i=1;i<nums.size()-1;i++){
-            if (nums[i]>nums[i-1] && nums[i]>nums[i+1]){
-                return i;
-            }
+        else{
+            return recursionBinary(nums,mid+1, high);
         }
         return -1;
+    }
+    int findPeakElement(vector<int>& nums) {
+        return recursionBinary(nums, 0, nums.size()-1);        
+        
     }
 };
