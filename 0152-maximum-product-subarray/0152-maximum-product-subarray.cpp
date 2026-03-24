ac04bf969d5a2b705b82c2d5fcbe2e5ca8067992
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2026-03-24 21:41:55 +0530

    Time: 367 ms (5.16%), Space: 17.6 MB (99.05%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -4,7 +4,7 @@ public:
         int prod=nums[0];
         for (int i=0;i<nums.size();i++){
             int temp=1;
-            for (int j=0;j<i;j++){
+            for (int j=i;j<nums.size();j++){
                 temp*=nums[j];
                 prod=max(prod,temp);
             }
