5cbc66967d1c9d9b72da83ec7fc9ee9c997d5f69
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2026-04-07 23:27:27 +0530

    Time: 0 ms (100%), Space: 16.3 MB (58.03%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,35 +1,49 @@
 class Solution {
 public:
-    int recursion(vector<vector<int>>& grid, int i, int j){
-        if (i<0 || j<0){
+    int recursion(vector<vector<int>>& grid, int i, int j) {
+        if (i < 0 || j < 0) {
             return 1e9;
         }
-        if (i==0 && j==0){
+        if (i == 0 && j == 0) {
             return grid[0][0];
         }
-        int res=grid[i][j];
-        return res+ min(recursion(grid,i-1,j),recursion(grid, i, j-1));
+        int res = grid[i][j];
+        return res + min(recursion(grid, i - 1, j), recursion(grid, i, j - 1));
     }
-    int solveMemo(int i,int j,vector<vector<int>>&grid,vector<vector<int>>&dp){
-        if(i<0 || j<0){ 
+    int solveMemo(int i, int j, vector<vector<int>>& grid,
+                  vector<vector<int>>& dp) {
+        if (i < 0 || j < 0) {
             return 1e9;
         }
-        if(i==0 && j==0){
+        if (i == 0 && j == 0) {
             return grid[0][0];
-        } ;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up=solveMemo(i-1,j,grid,dp);
-        int left=solveMemo(i,j-1,grid,dp);
-        return dp[i][j]=grid[i][j]+min(up,left);
+        };
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int up = solveMemo(i - 1, j, grid, dp);
+        int left = solveMemo(i, j - 1, grid, dp);
+        return dp[i][j] = grid[i][j] + min(up, left);
     }
     int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        //Recursion
-        // return recursion(grid,m-1,n-1);
+        int m = grid.size();
+        int n = grid[0].size();
+        // Recursion
+        //  return recursion(grid,m-1,n-1);
 
-        //Memoization
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solveMemo(m-1,n-1,grid,dp);
+        // Memoization
+        //  vector<vector<int>>dp(m,vector<int>(n,-1));
+        //  return solveMemo(m-1,n-1,grid,dp);
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 40001));
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 && j == 1) {
+                    dp[1][1] = grid[0][0];
+                } else
+                    dp[i][j] =
+                        grid[i - 1][j - 1] + min(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+        return dp[m][n];
     }
 };
