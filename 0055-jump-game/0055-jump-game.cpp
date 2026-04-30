c99a6237893733bcf236dddf23ebe92ea3e0c9bc
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2026-04-30 23:22:41 +0530

    Time: 0 ms (100%), Space: 52.2 MB (81.61%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        
+        //bruteforce
+        int curr=0;
+        for (int i=0;i<nums.size();i++){
+            curr=max(curr,nums[i]+i);
+            if (curr>=nums.size()-1){
+                return true;
+            }
+            if (curr<=i){
+                return false;
+            }
+        }
+        return false;
     }
 };
