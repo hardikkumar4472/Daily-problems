a3bea2204561d85302b752d32d64724c64390507
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2026-05-04 23:07:09 +0530

    Time: 274 ms (59.58%), Space: 285.5 MB (65.1%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,38 +1,34 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        // BFS
+        // int n=edges.size();
         vector<vector<int>> adj(n);
-        for (int i = 0; i < edges.size(); i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
+        for (int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-
-        vector<int> vis(n, 0);
+        vector<int> vis(n,0);
         queue<int> q;
         q.push(source);
-        vis[source] = 1;
-
-        while (!q.empty()) {
-            int node = q.front(); q.pop();
-            if (node == destination) return true;
-
-            for (int i = 0; i < adj[node].size(); i++) {
-                int nei = adj[node][i];
-                if (!vis[nei]) {
-                    vis[nei] = 1;
+        vis[source]=1;
+        while (!q.empty()){
+            int node=q.front();
+            q.pop();
+            if (node==destination){
+                return true;
+            }
+            for (int i=0;i<adj[node].size();i++){
+                int nei=adj[node][i];
+                if (!vis[nei]){
+                    vis[nei]=1;
                     q.push(nei);
                 }
             }
         }
         return false;
 
-
-        // DFS
+        
     }
 };
