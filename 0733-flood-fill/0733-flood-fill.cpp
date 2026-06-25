a4318fb2215dd47dd98a5931191ada3d126fe0ae
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2026-06-25 11:26:54 +0530

    Time: 0 ms (100%), Space: 18.6 MB (15.38%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& matrix, int sr, int sc, int color) {
-        vector<vector<int>>dirs={{-1,0},{1,0},{0,-1},{0,1}};
-        int row=matrix.size();
-        int col=matrix[0].size();
-        vector<vector<int>>ans;
-        ans=matrix;
-        int check=matrix[sr][sc];
-        queue<pair<int,int>>q;
-        vector<vector<int>>visited=matrix;
-        q.push({sr,sc});
-        visited[sr][sc]=-1;
-        ans[sr][sc]=color;
-        while(!q.empty()){
-            auto [r,c]=q.front();
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if (image[sr][sc] == color) return image;
+        vector<vector<int>> dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+        vector<vector<int>> result = image;
+        int m = image.size();
+        int n = image[0].size();
+        int check = image[sr][sc];
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+        result[sr][sc] = color; 
+        while (!q.empty()) {
+            auto [r, c] = q.front();
             q.pop();
-            for(auto d:dirs){
-                int nr=r+d[0];
-                int nc=c+d[1];
-                if(nr>=0 && nc>=0 && nr<row && nc<col &&
-                   visited[nr][nc]!=-1 &&
-                   matrix[nr][nc]==check){
-                    q.push({nr,nc});
-                    visited[nr][nc]=-1;
-                    ans[nr][nc]=color;
+            for (int i = 0; i < dirs.size(); i++) {
+                int nr = r + dirs[i][0]; 
+                int nc = c + dirs[i][1]; 
+                if (nr >= 0 && nc >= 0 && nr < m && nc < n && result[nr][nc] == check) {
+                    result[nr][nc] = color; 
+                    q.push({nr, nc});       
                 }
             }
         }
-        return ans;
+        return result;
     }
 };
