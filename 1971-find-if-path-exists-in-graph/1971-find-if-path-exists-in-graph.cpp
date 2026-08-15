5506ae171d6c3396b24d3e1d65000ae3228510a8
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2026-08-15 22:09:55 +0530

    Time: 337 ms (44.24%), Space: 305 MB (43.63%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+    bool dfs(vector<vector<int>>& adj, int source, int destination, vector<bool>& vis) {
+        
+        if (source == destination) {
+            return true;
+        }
+
+        vis[source] = true;
+
+        for (int i = 0; i < adj[source].size(); i++) {
+            int neighbor = adj[source][i];
+
+            if (!vis[neighbor]) {
+                if (dfs(adj, neighbor, destination, vis)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         vector<vector<int>> adj(n);
 
@@ -11,24 +31,28 @@ public:
             adj[v].push_back(u);
         }
             //BFS
-            queue<int> q;
-            vector<bool> vis(n,false);
-            q.push(source);
-            vis[source]=true;
-            while(!q.empty()){
-                int front=q.front();
-                q.pop();
-                if(front==destination){
-                    return true;
-                }
-                for (int i=0;i<adj[front].size();i++){
-                    int nei=adj[front][i];
-                    if(!vis[nei]){
-                        q.push(nei);
-                        vis[nei]=true;
-                    }
-                }
-        }
-        return false; 
+        //     queue<int> q;
+        //     vector<bool> vis(n,false);
+        //     q.push(source);
+        //     vis[source]=true;
+        //     while(!q.empty()){
+        //         int front=q.front();
+        //         q.pop();
+        //         if(front==destination){
+        //             return true;
+        //         }
+        //         for (int i=0;i<adj[front].size();i++){
+        //             int nei=adj[front][i];
+        //             if(!vis[nei]){
+        //                 q.push(nei);
+        //                 vis[nei]=true;
+        //             }
+        //         }
+        // }
+        // return false; 
+        vector<bool> vis(n, false);
+        return dfs(adj, source, destination, vis);
+
+
     }
 };
