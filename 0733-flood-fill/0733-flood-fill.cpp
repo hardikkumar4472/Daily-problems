e0d72231cf9d6b14e2590b774fa609586d01f7de
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2026-09-03 12:56:58 +0530

    Time: 0 ms (100%), Space: 19 MB (8.05%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if (image[sr][sc] == color) return image;
-        vector<vector<int>> dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
-        vector<vector<int>> result = image;
-        int m = image.size();
-        int n = image[0].size();
-        int check = image[sr][sc];
+        if(image[sr][sc]==color){
+            return image;
+        }
+        vector<vector<int>> vec;
+        vector<vector<bool>> vis(image.size(),vector<bool>(image[0].size(),false));
+        vec=image;
+        vec[sr][sc]=color;
+        vis[sr][sc]=true;
+        vector<vector<int>> dirs={{-1,0},{1,0},{0,-1},{0,1}};
         queue<pair<int, int>> q;
-        q.push({sr, sc});
-        result[sr][sc] = color; 
-        while (!q.empty()) {
-            auto [r, c] = q.front();
+        q.push({sr,sc});
+        int check=image[sr][sc];
+        while(!q.empty()){
+            int r=q.front().first;
+            int c=q.front().second;
             q.pop();
-            for (int i = 0; i < dirs.size(); i++) {
-                int nr = r + dirs[i][0]; 
-                int nc = c + dirs[i][1]; 
-                if (nr >= 0 && nc >= 0 && nr < m && nc < n && result[nr][nc] == check) {
-                    result[nr][nc] = color; 
-                    q.push({nr, nc});       
+            for (int i=0;i<dirs.size();i++){
+                int nr=r+dirs[i][0];
+                int nc=c+dirs[i][1];
+                if(nr>=0 && nc>=0 && nr<image.size() && nc<image[0].size() && image[nr][nc]==check && !vis[nr][nc] ){
+                    vec[nr][nc]=color;
+                    vis[nr][nc]=color;
+                    q.push({nr,nc});
                 }
             }
         }
-        return result;
+        return vec;
     }
 };
