37df56b958be9df8731b605489f82c64d4692f38
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2026-09-05 19:52:48 +0530

    Time: 7 ms (5.9%), Space: 17.1 MB (38.92%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,44 +1,43 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-          int n = grid.size();
-    int m = grid[0].size();
-    
-    queue<pair<int,int>> q;
-    int fresh = 0, time = 0;
-    
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(grid[i][j] == 2) q.push({i,j});
-            if(grid[i][j] == 1) fresh++;
-        }
-    }
-    
-    int dx[4] = {-1,1,0,0};
-    int dy[4] = {0,0,-1,1};
-    
-    while(!q.empty() && fresh){
-        int size = q.size();
-        time++;
-        
-        while(size--){
-            auto [x,y] = q.front();
-            q.pop();
-            
-            for(int k=0;k<4;k++){
-                int nx = x + dx[k];
-                int ny = y + dy[k];
-                
-                if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]==1){
-                    grid[nx][ny] = 2;
-                    fresh--;
-                    q.push({nx,ny});
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<int, int>> q;
+        int fresh=0;
+        int time=0;
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                if (grid[i][j]==2){
+                    q.push({i,j});
+                }
+                if(grid[i][j]==1){
+                    fresh++;
                 }
             }
         }
-    }
-    
-    if(fresh) return -1;
-    return time;
+        vector<vector<int>> dirs={{-1,0},{1,0},{0,-1},{0,1}};
+        while(!q.empty() && fresh!=0){
+            int size=q.size();
+            time++;
+            while(size--){
+                auto [x,y]=q.front();
+                q.pop();
+                for (int i=0;i<4;i++){
+                    int nx=x+dirs[i][0];
+                    int ny=y+dirs[i][1];
+                    if(nx>=0 && nx<n && ny>=0 && ny<m && grid[nx][ny]==1){
+                        grid[nx][ny]=2;
+                        fresh--;
+                        q.push({nx,ny});
+                    }
+                }
+            }
+        }
+        if (fresh){
+            return -1;
+        }
+        return time;
+        
     }
 };
